88ead29ec2394cfd471ec8fc46ace002a87c4688
albertosouza <alberto.souza.99@gmail.com>
2016-07-09 00:24:32 -0300

    remove option from model attribute text param

--- server/models/widget.js
+++ server/models/widget.js
@@ -48,7 +48,7 @@ module.exports = function Model(we) {
         defaultValue: 0
       },
       configuration: {
-        type: we.db.Sequelize.TEXT('medium'),
+        type: we.db.Sequelize.TEXT,
         skipSanitizer: true,
         get: function()  {
           if (this.getDataValue('configuration'))
